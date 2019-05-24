993d18200340c875310b16e9170eede3e9853752
Philippe Massicotte <pmassicotte@hotmail.com>
2019-05-24 13:22:45 -0400

    No test at the moment for gi_fetch_available_templates. I do not have a good idea on how to test it.

--- tests/testthat/test-gitignore_fetch_available_templates.R
+++
@@ -1,7 +0,0 @@
-# test_that("wrong url for the template breaks the code", {
-#   expect_error(
-#     gi_fetch_available_templates(
-#       url = "https://www.gitignoreeee.io/api/list?format=json"
-#     )
-#   )
-# })
